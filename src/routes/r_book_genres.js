2d786b9cc909e63279462c9b86262641abe7ca4a
rifanid98 <adninsijawa@gmail.com>
2020-06-11 22:16:42 +0700

    menambahkan multer

--- src/routes/r_book_genres.js
+++ src/routes/r_book_genres.js
@@ -6,6 +6,9 @@ const express = require('express');
 // instance of express router
 const router = express.Router();
 
+// import multer upload helper
+const upload = require('../helpers/upload');
+
 /**
  * Controllers
  */
@@ -15,9 +18,9 @@ const book_genres_controller = require('../controllers/c_book_genres');
 // Get All Books
 router.get('/', book_genres_controller.get_book_genres);
 // Post a Book
-router.post('/', book_genres_controller.post_book_genre);
+router.post('/', upload.none(), book_genres_controller.post_book_genre);
 // Patch a Book
-router.patch('/:id', book_genres_controller.patch_book_genre);
+router.patch('/:id', upload.none(), book_genres_controller.patch_book_genre);
 // Delete a Book
 router.delete('/:id', book_genres_controller.delete_book_genre);
 
