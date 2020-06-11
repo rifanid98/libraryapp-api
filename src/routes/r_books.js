7117bc55cc960b392c57b02125435d2dcbe52661
rifanid98 <adninsijawa@gmail.com>
2020-06-11 17:22:47 +0700

    refactoring some code

--- src/routes/r_books.js
+++ src/routes/r_books.js
@@ -6,43 +6,15 @@ const express = require('express');
 // instance of express router
 const router = express.Router();
 
+// import multer upload helper
+const upload = require('../helpers/upload');
+
 /**
  * Controllers
  */
 // load the controller
 const books_controller = require('../controllers/c_books');
 
-/**
- * Multer File Handling
- */
-// import multer
-const multer = require('multer');
-// import path
-const path = require('path');
-// set storage
-const storage = multer.diskStorage({
-    destination: path.join('src/assets/images/'),
-    filename: function (req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname);
-    }
-});
-// set file filter
-const fileFilter = function (req, file, cb) {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true);
-    } else {
-        // reject files
-        cb(null, false);
-    }
-};
-// set upload multer
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 20
-    },
-    fileFilter: fileFilter,
-});
 
 // Get All Books
 router.get('/', books_controller.get_books);
