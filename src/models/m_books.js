ee6f6b70aba8a5409e035d89bdae3c3fc58c6ba4
rifanid98 <adninsijawa@gmail.com>
2020-06-11 16:20:28 +0700

    adding function to get data from view

--- src/models/m_books.js
+++ src/models/m_books.js
@@ -5,8 +5,82 @@
  */
 const conn = require('../helpers/mysql');
 
+/**
+ * Custom Function
+ */
+function generate_like(filters) {
+    let result = "";
+    const length = Object.keys(filters).length - 1;
+    let i = 0;
+    for (key in filters) {
+        const filter = "'%" + filters[key] + "%'";
+        let field = "book_" + key;
+        if (key == "genre") {
+            field = "book_" + key + "_name";
+        }
+        result += (i == length) ? field + " LIKE " + filter : field + " LIKE " + filter + " OR ";
+        i++;
+    }
+    return result;
+}
+
 module.exports = {
-    
+    get_field_name: function() {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = `DESCRIBE books`;
+            conn.query(sqlQuery, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                let fields = [];
+                result.forEach(field => {
+                    fields.push(field['Field']);
+                });
+                resolve(fields);
+            })
+        })
+    },
+
+    get_data_custom: function (filters, total_data) {
+        return new Promise((resolve, reject) => {
+            let sqlQuery = "SELECT * FROM `v_book_and_genre` ";
+            
+            // search
+            if (Object.keys(filters.search).length > 0) {
+                sqlQuery += " WHERE " + generate_like(filters.search);
+            }
+
+            // sort
+            if (Object.keys(filters.sort).length > 0) {
+                sqlQuery += " ORDER BY book_" + filters.sort.sort + " ";
+            }
+            // pagination
+            if (Object.keys(filters.pagination).length > 0) {
+                let data_per_page = 5; 
+                let active_page = 1
+                
+                if ("limit" in filters.pagination) {
+                    data_per_page = filters.pagination.limit;
+                }
+                if ("page" in filters.pagination) {
+                    active_page = filters.pagination.page;
+                }
+                
+                let first_data = (data_per_page * active_page) - data_per_page;
+                sqlQuery += ("page" in filters.pagination) ? " LIMIT " + first_data + ", " + data_per_page + " " : "";
+            }
+            conn.query(sqlQuery, filters.search, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+            
+            // resolve(sqlQuery);
+            
+        })
+    },
+
     get_all_data: function() {
         return new Promise((resolve, reject) => {
             const sqlQuery = "SELECT * FROM books";
