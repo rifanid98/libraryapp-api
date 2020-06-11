abc2748420a38368d0ac70d26ad20d6beff3e20f
rifanid98 <adninsijawa@gmail.com>
2020-06-11 17:23:36 +0700

    upload helper

---
+++ src/helpers/upload.js
@@ -0,0 +1,34 @@
+
+/**
+ * Multer File Handling
+ */
+// import multer
+const multer = require('multer');
+// import path
+const path = require('path');
+// set storage
+const storage = multer.diskStorage({
+    destination: path.join('src/assets/images/'),
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+// set file filter
+const fileFilter = function (req, file, cb) {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        // reject files
+        cb(null, false);
+    }
+};
+// set upload multer
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 20
+    },
+    fileFilter: fileFilter,
+});
+
+module.exports = upload;
