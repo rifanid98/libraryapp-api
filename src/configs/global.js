1634c6164f3e1ddf025fec21093b009d5364bf16
rifanid98 <adninsijawa@gmail.com>
2020-06-12 23:05:05 +0700

    some configs added

--- src/configs/global.js
+++ src/configs/global.js
@@ -5,6 +5,11 @@ const config = {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
   },
+  jwtSecretKey: process.env.JWT_KEY,
+  rootProjectPath: 'libraryapp-api',
+  imageStaticPath: function(req) {
+    return `${req.protocol}://${req.get("host")}/${this.rootProjectPath}/images/`;
+  }
 };
 
 module.exports = config;
