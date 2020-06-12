eb16fbeedcc8770d155e1396e80e082be64a9324
rifanid98 <adninsijawa@gmail.com>
2020-06-12 17:08:09 +0700

    adding auth and users router

--- src/routes/r_index.js
+++ src/routes/r_index.js
@@ -11,11 +11,15 @@ const router = express.Router();
  */
 const books_router = require('./r_books');
 const book_genres_router = require('./r_book_genres');
+const auth_router = require('./r_auth');
+const users_router = require('./r_users');
 
 /**
  * Fire the router
  */
 router.use('/books', books_router);
 router.use('/book_genres', book_genres_router);
+router.use('/auth', auth_router);
+router.use('/users', users_router);
 
 module.exports = router;
