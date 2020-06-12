51f409767697cd38403bb8c28453fc9ddbe10462
rifanid98 <adninsijawa@gmail.com>
2020-06-12 17:10:11 +0700

    new model auth

---
+++ src/models/m_auth.js
@@ -0,0 +1,31 @@
+/**
+ * Config MySQL
+ * .
+ * load config mysql
+ */
+const conn = require('../helpers/mysql');
+
+module.exports = {
+    register: function (data) {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = "INSERT INTO users SET ?";
+            conn.query(sqlQuery, data, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+        })
+    },
+    login: function (data) {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = "SELECT * FROM users WHERE user_name = ?";
+            conn.query(sqlQuery, data, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+        })
+    }
+}
