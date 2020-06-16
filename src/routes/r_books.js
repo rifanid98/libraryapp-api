50a624d609b8f14133a206c8935b0fb940de682a
rifanid98 <adninsijawa@gmail.com>
2020-06-16 10:02:27 +0700

    some route changed

--- src/routes/r_books.js
+++ src/routes/r_books.js
@@ -21,14 +21,14 @@ const books_controller = require('../controllers/c_books');
 // Get All Books
 router.get('/', auth_middleware.level_user, books_controller.get_books);
 // Post a Book
-router.post('/', auth_middleware.level_staff, upload.single('book_image'), books_controller.post_book);
+router.post('/add', auth_middleware.level_staff, upload.single('book_image'), books_controller.post_book);
 // Return a Book
-router.patch('/return', auth_middleware.level_user, upload.single('book_image'), books_controller.return_book);
+router.patch('/return/:id', auth_middleware.level_user, upload.single('book_image'), books_controller.return_book);
 // Borrow a Book
-router.patch('/borrow', auth_middleware.level_user, upload.single('book_image'), books_controller.borrow_book);
+router.patch('/borrow/:id', auth_middleware.level_user, upload.single('book_image'), books_controller.borrow_book);
 // Patch a Book
-router.patch('/:id', auth_middleware.level_staff, upload.single('book_image'), books_controller.patch_book);
+router.patch('/update/:id', auth_middleware.level_staff, upload.single('book_image'), books_controller.patch_book);
 // Delete a Book
-router.delete('/:id', auth_middleware.level_admin, books_controller.delete_book);
+router.delete('/delete/:id', auth_middleware.level_admin, books_controller.delete_book);
 
 module.exports = router;
