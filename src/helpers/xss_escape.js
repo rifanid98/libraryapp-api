f1c37fc62d6d2125f50def81cf690ecf36e1dd5f
rifanid98 <adninsijawa@gmail.com>
2020-06-16 10:48:09 +0700

    xss escape validation

---
+++ src/helpers/xss_escape.js
@@ -0,0 +1,12 @@
+String.prototype.escape = function () {
+    var tagsToReplace = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        "'": "&apos;",
+        '"': "&quot;"
+    };
+    return this.replace(/[&<>\'\"]/g, function (tag) {
+        return tagsToReplace[tag] || tag;
+    });
+};
