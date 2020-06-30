3f63171a5e923b2743208189f5b76c29fda076c1
rifanid98 <adninsijawa@gmail.com>
2020-06-30 22:09:34 +0700

    get user by id, post user & patch user function added

--- src/controllers/c_users.js
+++ src/controllers/c_users.js
@@ -13,6 +13,12 @@ const usersModel = require("../models/m_users");
  */
 const myResponse = require("../helpers/myResponse");
 
+// import joi
+const validate = require('../helpers/joiSchema');
+
+// import bcrypt
+const bcrypt = require("bcrypt");
+
 // import custom error message
 const errorMessage = require("../helpers/myErrorMessage");
 
@@ -20,36 +26,138 @@ const errorMessage = require("../helpers/myErrorMessage");
  * CRUD
  */
 async function getUsers(req, res) {
-    try {
-        const result = await usersModel.getAllData();
-        for (key in result) {
-            delete result[key].password;
-        }
-        return myResponse.response(res, "success", result, 200, "Ok!")
-    } catch (error) {
-        console.log(error);
-        return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
-    }
+	try {
+		const result = await usersModel.getAllData();
+		for (key in result) {
+			delete result[key].password;
+		}
+		return myResponse.response(res, "success", result, 200, "Ok!")
+	} catch (error) {
+		console.log(error);
+		return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
+	}
+}
+
+async function getUserById(req, res) {
+	try {
+		const id = req.params.id;
+		const result = await usersModel.getDataById(id);
+		for (key in result) {
+			delete result[key].password;
+		}
+		return myResponse.response(res, "success", result, 200, "Ok!")
+	} catch (error) {
+		console.log(error);
+		return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
+	}
+}
+
+async function postUser(req, res) {
+	try {
+		// Joi validation
+		await validate.validateUsers(req.body);
+
+		const data = req.body;
+		let checkUser = 0;
+		const checkUsername = await usersModel.getDataByUsername(data.username);
+		const checkEmail = await usersModel.getDataByEmail(data.email);
+
+		checkUser += checkUsername.length;
+		checkUser += checkEmail.length;
+
+		if (checkUser > 0) {
+			const message = `Duplicate data ${data.username} or ${data.email}`;
+			return myResponse.response(res, "failed", "", 409, message);
+		}
+
+		const salt = bcrypt.genSaltSync(10);
+		const hash = bcrypt.hashSync(data.password, salt);
+		data.password = hash;
+
+		const result = await usersModel.addData(data);
+		if (result.affectedRows > 0) {
+			delete data.password;
+			const newData = {
+				user_id: result.insertId,
+				...data
+			}
+			return myResponse.response(res, "success", newData, 200, "Ok!")
+		} else {
+			const message = `Add data failed`;
+			return myResponse.response(res, "failed", "", 409, message);
+		}
+	} catch (error) {
+		console.log(error);
+		return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
+	}
+}
+
+async function patchUser(req, res) {
+	try {
+		// Joi validation
+		const fieldToPatch = Object.keys(req.body);
+		await validate.validateUsers(req.body, fieldToPatch);
+
+		const data = req.body;
+		const id = req.params.id;
+
+		let checkUser = 0;
+		const checkUsername = await usersModel.getDataByUsername(data.username);
+		const checkEmail = await usersModel.getDataByEmail(data.email);
+
+		checkUser += checkUsername.length;
+		checkUser += checkEmail.length;
+
+		if (checkUser > 0) {
+			const message = `Duplicate data ${data.username} or ${data.email}`;
+			return myResponse.response(res, "failed", "", 409, message);
+		}
+
+		if (data.password) {
+			const salt = bcrypt.genSaltSync(10);
+			const hash = bcrypt.hashSync(data.password, salt);
+			data.password = hash;
+		}
+
+		const result = await usersModel.updateData(data, id);
+		if (result.affectedRows > 0) {
+			data.password && delete data.password;
+			const newData = {
+				user_id: result.insertId,
+				...data
+			}
+			return myResponse.response(res, "success", newData, 200, "Ok!")
+		} else {
+			const message = `Add data failed`;
+			return myResponse.response(res, "failed", "", 409, message);
+		}
+	} catch (error) {
+		console.log(error);
+		return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
+	}
 }
 
 async function deleteUser(req, res) {
-    try {
-        const id = req.params.id;
-        const result = await usersModel.deleteDataById(id);
+	try {
+		const id = req.params.id;
+		const result = await usersModel.deleteDataById(id);
 
-        if (result.affectedRows > 0) {
-            return myResponse.response(res, "success", { user_id: id }, 200, "Deleted!")
-        } else {
-            const message = `Data with id ${id} is not found`;
-            return myResponse.response(res, "failed", "", 404, message);
-        }
-    } catch (error) {
-        console.log(error);
-        return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
-    }
+		if (result.affectedRows > 0) {
+			return myResponse.response(res, "success", { user_id: id }, 200, "Deleted!")
+		} else {
+			const message = `Data with id ${id} is not found`;
+			return myResponse.response(res, "failed", "", 404, message);
+		}
+	} catch (error) {
+		console.log(error);
+		return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
+	}
 }
 
 module.exports = {
-    deleteUser,
-    getUsers
+	deleteUser,
+	getUsers,
+	postUser,
+	patchUser,
+	getUserById
 }
