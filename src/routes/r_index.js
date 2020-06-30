23349fedafe7627e992eaaf901916e0357ad52dc
rifanid98 <adninsijawa@gmail.com>
2020-06-30 18:03:13 +0700

    author route added

--- src/routes/r_index.js
+++ src/routes/r_index.js
@@ -14,15 +14,17 @@ const validationMiddleware = require('../middlewares/mdl_validation');
  * Load All Routes
  */
 const booksRouter = require('./r_books');
-const bookGenresRouter = require('./r_bookGenres');
+const genresRouter = require('./r_genres');
+const authorRouter = require('./r_author');
 const authRouter = require('./r_auth');
 const usersRouter = require('./r_users');
 
 /**
  * Fire the router
  */
-router.use('/books', validationMiddleware.xssEscape , authMiddleware.verifyJwtToken, booksRouter);
-router.use('/books/genres', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, bookGenresRouter);
+router.use('/books', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, booksRouter);
+router.use('/genres', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, genresRouter);
+router.use('/authors', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, authorRouter);
 router.use('/auth', validationMiddleware.xssEscape, authRouter);
 router.use("/users", validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, usersRouter);
 
