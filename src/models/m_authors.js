736c80feda82625dca951a8d6285505a74eb15b9
rifanid98 <adninsijawa@gmail.com>
2020-06-30 18:03:28 +0700

    author model added

---
+++ src/models/m_authors.js
@@ -0,0 +1,83 @@
+/**
+ * Config MySQL
+ * .
+ * load config mysql
+ */
+const conn = require('../helpers/mysql');
+
+module.exports = {
+
+    getAllData: function () {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = "SELECT * FROM authors";
+            conn.query(sqlQuery, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+        })
+    },
+
+    getDataByName: function (data) {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = "SELECT * FROM authors WHERE ? ";
+
+            conn.query(sqlQuery, data, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+        })
+    },
+
+    getDataById: function (id) {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = "SELECT * FROM authors WHERE author_id = ? ";
+
+            conn.query(sqlQuery, id, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+        })
+    },
+
+    addData: function (data) {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = "INSERT INTO authors SET ?";
+            conn.query(sqlQuery, data, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+        })
+    },
+
+    updateData: function (data, id) {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = "UPDATE authors SET ? WHERE author_id = ?";
+            conn.query(sqlQuery, [data, id], function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+        })
+    },
+
+    deleteData: function (id) {
+        return new Promise((resolve, reject) => {
+            const sqlQuery = "DELETE FROM authors WHERE author_id = ?";
+            conn.query(sqlQuery, id, function (error, result) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            })
+        })
+    },
+}
