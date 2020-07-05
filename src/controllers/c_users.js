990663b6b66a03c169176e507ebfb9a3c6574edc
rifanid98 <adninsijawa@gmail.com>
2020-07-05 16:39:49 +0700

    fix file handling & add image upload

--- src/controllers/c_users.js
+++ src/controllers/c_users.js
@@ -19,8 +19,14 @@ const validate = require('../helpers/joiSchema');
 // import bcrypt
 const bcrypt = require("bcrypt");
 
+// import config
+const config = require('../configs/global');
+
 // import custom error message
 const errorMessage = require("../helpers/myErrorMessage");
+
+// delete image
+const deleteImage = require("../helpers/deleteImage");
 
 /**
  * CRUD
@@ -41,39 +47,74 @@ async function getUsers(req, res) {
 async function postUser(req, res) {
 	try {
 		// Joi validation
-		await validate.validateUsers(req.body);
+		const fieldToPatch = Object.keys(req.body);
+		await validate.validateUsers(req.body, fieldToPatch);
 
-		const data = req.body;
+		const body = req.body;
 		let checkUser = 0;
-		const checkUsername = await usersModel.getDataByUsername(data.username);
-		const checkEmail = await usersModel.getDataByEmail(data.email);
+		const checkUsername = await usersModel.getDataByUsername(body.username);
+		const checkEmail = await usersModel.getDataByEmail(body.email);
 
 		checkUser += checkUsername.length;
 		checkUser += checkEmail.length;
 
 		if (checkUser > 0) {
-			const message = `Duplicate data ${data.username} or ${data.email}`;
+			if (req.file) {
+				// delete new image when duplicated data
+				const myRequest = { protocol: req.protocol, host: req.get('host') }
+				deleteImage.delete(myRequest, req.file.filename);
+			}
+
+			const message = `Duplicate data ${body.username} or ${body.email}`;
 			return myResponse.response(res, "failed", "", 409, message);
 		}
 
-		const salt = bcrypt.genSaltSync(10);
-		const hash = bcrypt.hashSync(data.password, salt);
-		data.password = hash;
-
-		const result = await usersModel.addData(data);
-		if (result.affectedRows > 0) {
-			delete data.password;
-			const newData = {
-				user_id: result.insertId,
-				...data
-			}
-			return myResponse.response(res, "success", newData, 200, "Ok!")
+		if (req.file === undefined) {
+			// set default file when no image to upload
+			body.image = `${config.imageUrlPath(req)}avatar.png`;
 		} else {
+			if (req.file.mimetype === 'image/jpeg' || req.file.mimetype === 'image/png') {
+				// get the image name and set into data
+				body.image = `${config.imageUrlPath(req)}${req.file.filename}`;
+			} else {
+				// delete new file when not an image
+				const myRequest = { protocol: req.protocol, host: req.get('host') }
+				deleteImage.delete(myRequest, req.file.filename);
+
+				const message = `There is no image to upload`;
+				return myResponse.response(res, "failed", "", 500, message);
+			}
+		}
+
+		const salt = bcrypt.genSaltSync(10);
+		const hash = bcrypt.hashSync(body.password, salt);
+		body.password = hash;
+
+		const add = await usersModel.addData(body);
+		if (add.affectedRows > 0) {
+			delete body.password;
+			const result = {
+				user_id: add.insertId,
+				...body
+			}
+			return myResponse.response(res, "success", result, 200, "Ok!")
+		} else {
+			if (req.file) {
+				// delete new image when insert data is failed
+				const myRequest = { protocol: req.protocol, host: req.get('host') }
+				deleteImage.delete(myRequest, req.file.filename);
+			}
+
 			const message = `Add data failed`;
 			return myResponse.response(res, "failed", "", 409, message);
 		}
 	} catch (error) {
 		console.log(error);
+		if (req.file) {
+			// delete new image when insert data is failed
+			const myRequest = { protocol: req.protocol, host: req.get('host') }
+			deleteImage.delete(myRequest, req.file.filename);
+		}
 		return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
 	}
 }
@@ -84,19 +125,65 @@ async function patchUser(req, res) {
 		const fieldToPatch = Object.keys(req.body);
 		await validate.validateUsers(req.body, fieldToPatch);
 
-		const data = req.body;
+		const body = req.body;
 		const id = req.params.id;
 
-		let checkUser = 0;
-		const checkUsername = await usersModel.getDataByUsername(data.username);
-		const checkEmail = await usersModel.getDataByEmail(data.email);
+		// checking if data is exists or not
+		const oldData = await usersModel.getDataById(id);
+		if (oldData.length < 1) {
+			if (req.file) {
+				// delete new image when duplicated data
+				const myRequest = { protocol: req.protocol, host: req.get('host') }
+				deleteImage.delete(myRequest, req.file.filename);
+			}
 
-		checkUser += checkUsername.length;
-		checkUser += checkEmail.length;
+			const message = `Data with id ${id} not found`;
+			return myResponse.response(res, "failed", "", 404, message);
+		}
 
-		if (checkUser > 0) {
-			const message = `Duplicate data ${data.username} or ${data.email}`;
+		let userDuplicate = 0;
+		const checkUser = await usersModel.getDataByField({ username: body.username });
+		checkUser.length > 0
+			&& checkUser[0].user_id !== parseInt(id)
+			? userDuplicate += checkUser.length : null;
+		const checkEmail = await usersModel.getDataByField({ email: body.email });
+		checkEmail.length > 0
+			&& checkEmail[0].user_id !== parseInt(id)
+			? userDuplicate += checkEmail.length : null;
+
+		if (userDuplicate > 0) {
+			if (req.file) {
+				// delete new image when duplicated data
+				const myRequest = { protocol: req.protocol, host: req.get('host') }
+				deleteImage.delete(myRequest, req.file.filename);
+			}
+
+			const message = `Duplicate data ${body.username} or ${body.email}`;
 			return myResponse.response(res, "failed", "", 409, message);
+		}
+
+		// checking is there a file or not
+		let data = {};
+		if (req.file === undefined) {
+			// if there is no file to upload
+			data = {
+				...body
+			};
+		} else {
+			// checking type of file
+			if (req.file.mimetype === 'image/jpeg' || req.file.mimetype === 'image/png') {
+				data = {
+					...body,
+					image: `${config.imageUrlPath(req)}${req.file.filename}`,
+				};
+			} else {
+				// delete new file when not an image
+				const myRequest = { protocol: req.protocol, host: req.get('host') }
+				deleteImage.delete(myRequest, req.file.filename);
+
+				const message = `There is no image to upload`;
+				return myResponse.response(res, "failed", "", 500, message);
+			}
 		}
 
 		if (data.password) {
@@ -105,20 +192,40 @@ async function patchUser(req, res) {
 			data.password = hash;
 		}
 
-		const result = await usersModel.updateData(data, id);
-		if (result.affectedRows > 0) {
+		const update = await usersModel.updateData(data, id);
+		if (update.affectedRows > 0) {
+			const imageName = oldData[0].image.split('/').pop();
+			if (imageName != 'avatar.png' && req.file !== undefined) {
+				// delete old image when not default image
+				const myRequest = { protocol: req.protocol, host: req.get('host') }
+				deleteImage.delete(myRequest, oldData[0].image);
+			}
+
 			data.password && delete data.password;
-			const newData = {
-				user_id: result.insertId,
+			const result = {
+				user_id: update.insertId,
 				...data
 			}
-			return myResponse.response(res, "success", newData, 200, "Ok!")
+			return myResponse.response(res, "success", result, 200, "Ok!")
 		} else {
-			const message = `Add data failed`;
+			if (req.file) {
+				// delete new image when duplicated data
+				const myRequest = { protocol: req.protocol, host: req.get('host') }
+				deleteImage.delete(myRequest, req.file.filename);
+			}
+
+			const message = `Update data failed`;
 			return myResponse.response(res, "failed", "", 409, message);
 		}
 	} catch (error) {
 		console.log(error);
+
+		// delete image when error
+		if (req.file) {
+			const myRequest = { protocol: req.protocol, host: req.get('host') }
+			deleteImage.delete(myRequest, req.file.filename);
+		}
+
 		return myResponse.response(res, "failed", "", 500, errorMessage.myErrorMessage(error, {}));
 	}
 }
